30b6f5e0dcc46cb2feeef3cbd99f5122573729ff
İbrahim Uyanık <ibrahimhakkiuyanik@outlook.com>
2022-10-06 17:50:05 +0300

    2.Ders Sonu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component({ // C#'daki attribute ile aynı şeydir @Component ts'de bunlara deklarasyon denir.
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
