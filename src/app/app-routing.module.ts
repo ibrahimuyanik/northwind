25c1b2599e5a667f640eb76f04646749b8e61fe7
İbrahim Uyanık <ibrahimhakkiuyanik@outlook.com>
2022-10-21 00:46:29 +0300

    20. Ders Sonu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductAddComponent } from './components/product-add/product-add.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
   {path:"", pathMatch:"full",component:ProductComponent},
   {path:"products", component:ProductComponent},
-  {path:"products/category/:categoryId", component:ProductComponent} // :categoryId --> categoryId değişken demek neye basılırsa onun id'si gelecek demek products/category sabit link yani değişmeyecek :categoryId değişken demek 
+  {path:"products/category/:categoryId", component:ProductComponent}, // :categoryId --> categoryId değişken demek neye basılırsa onun id'si gelecek demek products/category sabit link yani değişmeyecek :categoryId değişken demek 
+  {path:"products/add", component:ProductAddComponent},
 ];
 
 @NgModule({
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
 import { Product } from '../models/product';
+import { ResponseModel } from '../models/responseModel';
 
 
 
@@ -25,6 +26,10 @@ export class ProductService {
   getProductsByCategory(categoryId:number): Observable<ListResponseModel<Product>> {
     let newPath = this.apiUrl + "products/getbycategory?categoryId=" + categoryId;
     return this.httpClient.get<ListResponseModel<Product>>(newPath)
+  }
+
+  add(product:Product):Observable<ResponseModel>{ // Ürün eklediğimiz zaman api'den bize success ve message bilgileri döndüğü için ResponseModel tipinde olmasını söyledik
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add", product)
   }
 
 }
